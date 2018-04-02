59664d7b6589664df9db28cbfe9630d522ba1658
yunzhongxiaoma <i_need_you0.0@qq.com>
2018-04-02 13:59:32 +0800

    添加plan模块，修改time模块
    1.	更改activity_time.xml的id命名，使之符合规范
    2.	对activity_time.xml界面做了较大调整，去掉 “android:id="@+id/et_input"”的文本框、“获取倒计时时间”按钮、“开始计时”按钮
    3.	添加plan模块
    4.	找到把.png 图片放到drawable-v21文件夹下（在API 21 以下的手机运行时）不能运行的bug，把图片移到drawable下就好了
    具体查看 “问题汇总.docx”:报错Resource is not a Drawable (color or path):TypedValue{t=0x1/d=0x7f02004a a=-1 r=0x7f02004a}
    
    明日计划：
    1. 商议确定图片命名规范
    2. 要不要plan模块的”提醒“功能，利弊分析
    3. 商议设计time模块

--- app/src/main/java/com/ghost/soulmate/MainActivity.java
+++ app/src/main/java/com/ghost/soulmate/MainActivity.java
@@ -6,29 +6,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.ghost.soulmate.R;
 
 public class MainActivity extends Activity{
     int[] jokes = {R.string.joke1, R.string.joke2, R.string.joke3, R.string.joke4, R.string.joke5};
-
-    ImageButton cat;
-    Button timer;
+    public static final String Action_main = "android.intent.action.MAIN";
+    ImageView cat;
+    ImageButton feed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cat = (ImageButton)findViewById(R.id.img);
-        timer = (Button)findViewById(R.id.btn_start);
-        //设置点击跳转
-        timer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(CountTime.Action_time));
-
-            }
-        });
+        cat = (ImageView)findViewById(R.id.ibtn_mainCat);
+        feed = (ImageButton)findViewById(R.id.ibtn_Feed);
 
         //为按钮的单击事件绑定事件监听器       --此方法为匿名内部类
         cat.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +30,16 @@ public class MainActivity extends Activity{
                 //创建一个Toast提示信息
                 int randomNum = (int)(Math.random()*5);
                 Toast.makeText(MainActivity.this, jokes[randomNum], Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // 转到计划模块
+        feed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PlanActivity.Action_plan);
+                // 启动intent对应的Activity
+                startActivity(intent);
             }
         });
     }
